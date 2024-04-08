6ed6e28c7cf48377d3108205cfb119c69cedf3f7
Jhun Kusano <jhun@jhun.com.br>
2024-04-08 13:23:47 -0300

    - esp_reset_reason() added to debug

--- display.ino
+++ display.ino
@@ -21,6 +21,11 @@ extern double Vrms1;
 extern double Vrms2;
 extern double Vrms3;
 
+extern String motivoReboot;
+extern String horaReboot;
+
+
+
 void startDisplay(){
   Wire.begin(5, 4);// Start I2C Communication SDA = 5 and SCL = 4 on Wemos Lolin32 ESP32 with built-in SSD1306 OLED
 
@@ -32,26 +37,40 @@ void startDisplay(){
   display.clearDisplay();
 }
 
+float incremento = 0.00;
 void showDisplayData(){
+  incremento += 0.01;
   display.clearDisplay();
 
   display.setTextSize(1);
   display.setTextColor(WHITE);
   display.setCursor(0, 0);
-  display.println("Corrente 1: " + String(Irms1) + "A");
+  // display.println("Corrente 1: " + String(Irms1) + "A");
+  // display.setCursor(0, 10);
+  // display.println("Corrente 2: " + String(Irms2) + "A");
+  // display.setCursor(0, 20);
+  // display.println("Corrente 3: " + String(Irms3) + "A");
+  // display.setCursor(0, 30);
+  // display.println("Tensao 1: " + String(Vrms1) + "V");
+  // display.setCursor(0, 40);
+  // display.println("Tensao 2: " + String(Vrms2) + "V");
+  // display.setCursor(0, 50);
+  // display.println("Tensao 3: " + String(Vrms3) + "V");
+  display.setCursor(0, 0);
+  display.println(horaReboot + ": ");
   display.setCursor(0, 10);
-  display.println("Corrente 2: " + String(Irms2) + "A");
+  display.println(motivoReboot);
   display.setCursor(0, 20);
-  display.println("Corrente 3: " + String(Irms3) + "A");
+  display.println("Tensao 1: " + String(Vrms1+incremento) + "V");
   display.setCursor(0, 30);
-  display.println("Tensao 1: " + String(Vrms1) + "V");
-  display.setCursor(0, 40);
   display.println("Tensao 2: " + String(Vrms2) + "V");
-  display.setCursor(0, 50);
+  display.setCursor(0, 40);
   display.println("Tensao 3: " + String(Vrms3) + "V");
+
  
   display.display();
 }
+
 
 void showConnectionsData(String ssid,String password, String ipAP, String wwwuser, String wwwpass){
   display.clearDisplay();
--- esp-master-newwifi-tensao-corrente.ino
+++ esp-master-newwifi-tensao-corrente.ino
@@ -11,15 +11,91 @@ extern void startDisplay();
 extern void showDisplayData();
 extern void showConnectionsData();
 
+esp_reset_reason_t BootReason;
+String motivoReboot;
+String horaReboot;
+
 WiFiManager wifi_manager;
 
 void setup()
 {
     Serial.begin(115200);
-    WiFi.begin("pedrabranca_automacao","@utoP3dra");
+    startDisplay();
+    BootReason = esp_reset_reason();
+    WiFi.begin("MR.ROBOT","Senh@1q2w3e");
     startConnection();
     startEnergyMonitor();
-    startDisplay();
+
+    struct tm timeinfo;
+    getLocalTime(&timeinfo);
+    char datetimeReboot[30];
+    size_t s = strftime(datetimeReboot, sizeof(datetimeReboot), "%Y-%m-%dT%H:%M:%S", &timeinfo);
+    horaReboot= String(datetimeReboot);
+    Serial.print(horaReboot+": ");
+    
+    switch (BootReason) {
+        case ESP_RST_UNKNOWN:
+          motivoReboot = "Reset reason can not be determined";
+          Serial.println(motivoReboot);
+        break;
+
+        case ESP_RST_POWERON:
+
+          motivoReboot = "Reset due to power-on event";
+          Serial.println(motivoReboot);
+        break;
+
+        case ESP_RST_EXT:
+          motivoReboot = "Reset by external pin (not applicable for ESP32)";
+          Serial.println(motivoReboot);
+        break;
+
+        case ESP_RST_SW:
+          motivoReboot = "Software reset via esp_restart";
+          Serial.println(motivoReboot);
+        break;
+
+        case ESP_RST_PANIC:
+          motivoReboot = "Software reset due to exception/panic";
+          Serial.println(motivoReboot);
+        break;
+
+        case ESP_RST_INT_WDT:
+          motivoReboot = "Reset (software or hardware) due to interrupt watchdog";
+          Serial.println(motivoReboot);
+        break;
+
+        case ESP_RST_TASK_WDT:
+          motivoReboot = "Reset due to task watchdog";
+          Serial.println(motivoReboot);
+        break;
+
+        case ESP_RST_WDT:
+          motivoReboot = "Reset due to other watchdogs";
+          Serial.println(motivoReboot);
+        break;                                
+
+        case ESP_RST_DEEPSLEEP:
+          motivoReboot = "Reset after exiting deep sleep mode";
+          Serial.println(motivoReboot);
+        break;
+
+        case ESP_RST_BROWNOUT:
+          motivoReboot = "Brownout reset (software or hardware)";
+          Serial.println(motivoReboot);
+        break;
+        
+        case ESP_RST_SDIO:
+          motivoReboot = "Reset over SDIO";
+          Serial.println(motivoReboot);
+        break;
+        
+        default:
+        break;
+    }
+
+   
+
     while(wifi_manager.apIsActive){
       showConnectionsData(String((char *)currentConfig.ssid),String((char *)currentConfig.password),String((char *)currentConfig.ipAP),String((char *)currentConfig.wwwuser),String((char *)currentConfig.wwwpass));
       delay(1000);
